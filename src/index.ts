04984accfd241da852f0012efb14868d5585a386
caseybrookes <caseybrookes828@gmail.com>
2024-09-20 22:57:53 -0400

    fix(practs): bump practs to latest best

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,22 @@ export class EventStream<T> {
   constructor() {
     this.emitter = new EventEmitter();
   }
-  public publish = (event: T) => {
+  public publish = (event: T): void => {
     this.emitter.emit('event', event);
   };
-  public subscribe = ({ consumer }: { consumer: EventStreamConsumer<T> }) => {
+  public subscribe = ({
+    consumer,
+  }: {
+    consumer: EventStreamConsumer<T>;
+  }): void => {
     // subscribe consumer to topic
     this.emitter.on('event', consumer);
   };
-  public unsubscribe = ({ consumer }: { consumer: EventStreamConsumer<T> }) => {
+  public unsubscribe = ({
+    consumer,
+  }: {
+    consumer: EventStreamConsumer<T>;
+  }): void => {
     // unsubscribe consumer from topic
     this.emitter.removeListener('event', consumer);
   };
